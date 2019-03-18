e727a9af2679da8b88d785fcd3e24258c26ece24
meooow25 <soumiksarkar.3120@gmail.com>
2019-03-19 03:34:10 +0530

    Update string hashing

--- StringHash.cpp
+++ StringHash.cpp
@@ -1,69 +1,65 @@
-
 // Polynomial hash for strings
 // hash(s) = s[0] + s[1] * b + s[2] * b^2 + ...
-// Uses double hash to decrease collision chances
+// Double hash
 
-long long powmod(long long a, long long b, long long m) {
-    long long r = 1;
-    while (b) {
-        if (b & 1) r = r * a % m;
-        b >>= 1; a = a * a % m;
-    }
-    return r;
-}
+// Refs:
+// https://cp-algorithms.com/string/string-hashing.html
+// https://codeforces.com/blog/entry/60445
+// http://codeforces.com/blog/entry/4898
+// https://codeforces.com/blog/entry/60442
 
-struct hpair {
-    long long x, y;
-    hpair(): x(0), y(0) {}
-    hpair(long long _x, long long _y): x(_x), y(_y) {}
-    hpair operator+(hpair p) { return hpair(x + p.x, y + p.y); }
-    hpair operator-(hpair p) { return hpair(x - p.x, y - p.y); }
-    hpair operator*(hpair p) { return hpair(x * p.x, y * p.y); }
-    hpair operator%(hpair p) { return hpair(x % p.x, y % p.y); }
-    bool operator==(hpair p) { return x == p.x && y == p.y; }
-    bool operator!=(hpair p) { return x != p.x || y != p.y; }
-};
-const hpair B(257, 259), M(1e9 + 7, 1e9 + 9);
+// Problems:
+// https://www.spoj.com/problems/NHAY/
 
-struct strhash {
-    vector<hpair> hash, bpow, bipow;
-
-    inline hpair inv(hpair a, hpair m) {
-        return hpair(powmod(a.x, m.x - 2, m.x), powmod(a.y, m.y - 2, m.y));
+namespace StrHash {
+    long long powmod(long long a, long long b, long long m) {
+        long long r = 1;
+        while (b) {
+            if (b & 1) r = r * a % m;
+            b >>= 1; a = a * a % m;
+        }
+        return r;
     }
 
-    strhash(char *s, int n): hash(n + 1), bpow(n + 1), bipow(n + 1) {
+    struct hpair {
+        long long x, y;
+        hpair(): x(0), y(0) {}
+        hpair(long long x_): x(x_), y(x_) {}
+        hpair(long long _x, long long _y): x(_x), y(_y) {}
+        hpair operator+(const hpair &p) const { return hpair(x + p.x, y + p.y); }
+        hpair operator-(const hpair &p) const { return hpair(x - p.x, y - p.y); }
+        hpair operator*(const hpair &p) const { return hpair(x * p.x, y * p.y); }
+        hpair operator%(const hpair &p) const { return hpair(x % p.x, y % p.y); }
+        bool operator==(const hpair &p) const { return x == p.x && y == p.y; }
+        bool operator!=(const hpair &p) const { return x != p.x || y != p.y; }
+        bool operator<(const hpair &p) const { return x == p.x ? y < p.y : x < p.x; }
+        hpair inv(const hpair &m) const {
+            return hpair(powmod(x, m.x - 2, m.x), powmod(y, m.y - 2, m.y));
+        }
+    };
+    const hpair B(257, 263), M(1e9 + 7, 1e9 + 9);
+
+    vector<hpair> bpow, bipow;
+
+    void init(int maxlen) {
+        bpow.resize(maxlen + 1), bipow.resize(maxlen + 1);
         bpow[0] = bipow[0] = {1, 1};
-        hpair binv = inv(B, M);
-        for (int i = 1; i <= n; i++) {
-            hash[i] = (hash[i - 1] + hpair(s[i - 1], s[i - 1]) * bpow[i - 1]) % M;
-            bpow[i] = bpow[i - 1] * B % M, bipow[i] = bipow[i - 1] * binv % M;
+        hpair binv = B.inv(M);
+        for (int i = 1; i <= maxlen; i++) {
+            bpow[i] = bpow[i - 1] * B % M;
+            bipow[i] = bipow[i - 1] * binv % M;
         }
     }
 
-    inline hpair get(int i, int j) {
-        return (hash[j + 1] - hash[i] + M) * bipow[i] % M;
-    }
-};
-
-struct strhash_lowmemory {
-    vector<hpair> hash;
-
-    inline hpair inv(hpair a, hpair m) {
-        return hpair(powmod(a.x, m.x - 2, m.x), powmod(a.y, m.y - 2, m.y));
+    template <typename T>
+    vector<hpair> make(const T &s, int n) {
+        vector<hpair> hash(n + 1);
+        for (int i = 1; i <= n; i++)
+            hash[i] = (hash[i - 1] + hpair(s[i - 1]) * bpow[i - 1]) % M;
+        return hash;
     }
 
-    strhash(char *s, int n) {
-        hash.resize(n + 1);
-        hpair bpow(1, 1);
-        for (int i = 1; i <= n; i++) {
-            hash[i] = (hash[i - 1] + hpair(s[i - 1], s[i - 1]) * bpow) % M;
-            bpow = bpow * B % M;
-        }
+    hpair subhash(vector<hpair> &hash, int l, int r) {
+        return (hash[r + 1] - hash[l] + M) * bipow[l] % M;
     }
-
-    inline hpair get(int i, int j) {
-        hpair bpow(powmod(B.x, i, M.x), powmod(B.y, i, M.y));
-        return (hash[j + 1] - hash[i] + M) * inv(bpow, M) % M;
-    }
-};
+}
