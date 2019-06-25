fba56d97a3d1ac4f522c29dabadc49667771098b
Soumik Sarkar <soumiksarkar.3120@gmail.com>
2019-06-26 03:25:26 +0530

    Improve lazy segment tree

--- SegtreeLazy.cpp
+++ SegtreeLazy.cpp
@@ -6,52 +6,52 @@ struct SegTree {
     };
     vector<node> t;
 
-    SegTree(int n): t(n << 2) {
-        build(1, 0, n);
+    SegTree(int n): t(n * 4) {
+        build(1, 1, n);
     }
 
     void build(int i, int s, int e) {
         t[i].s = s, t[i].e = e;
         if (s == e) { t[i].v = T::at(s); return; }
-        int m = (s + e) >> 1;
-        build(i << 1, s, m); build(i << 1 | 1, m + 1, e);
-        t[i].v = T(t[i << 1].v, t[i << 1 | 1].v);
+        int m = (s + e) / 2;
+        build(i * 2, s, m); build(i * 2 + 1, m + 1, e);
+        t[i].v = T(t[i * 2].v, t[i * 2 + 1].v);
     }
 
     void push(int i) {
-        if (t[i].s < t[i].e) t[i].v.push(t[i << 1].v, t[i << 1 | 1].v);
+        if (t[i].s < t[i].e) t[i].v.push(t[i * 2].v, t[i * 2 + 1].v);
         t[i].v.apply();
     }
 
-    void update(int i, int us, int ue, auto v) {
+    void update(int i, int us, int ue, const auto& v) {
         push(i);
         if (ue < t[i].s || t[i].e < us) return;
         if (us <= t[i].s && t[i].e <= ue) { t[i].v.store(v); push(i); return; }
-        update(i << 1, us, ue, v); update(i << 1 | 1, us, ue, v);
-        t[i].v = T(t[i << 1].v, t[i << 1 | 1].v);
+        update(i * 2, us, ue, v); update(i * 2 + 1, us, ue, v);
+        t[i].v = T(t[i * 2].v, t[i * 2 + 1].v);
     }
 
     T query(int i, int qs, int qe) {
         if (qe < t[i].s || t[i].e < qs) return T();
         push(i);
         if (qs <= t[i].s && t[i].e <= qe) return t[i].v;
-        return T(query(i << 1, qs, qe), query(i << 1 | 1, qs, qe));
+        return T(query(i * 2, qs, qe), query(i * 2 + 1, qs, qe));
     }
 };
 
-struct data {
-    int len; long long sum, lazy;
-    data(): len(0), sum(0), lazy(0) {}
-    data(int x): len(1), sum(x), lazy(0) {}
-    static data at(int i) {
-        return data(0);
+// Example for +delta on range and sum queries
+struct Data {
+    int len = 1; long long sum, lazy = 0;
+    Data() = default;
+    Data(int x): sum(x) {}
+    static Data at(int i) {
+        return Data(0);
     }
-    data(data l, data r) {
+    Data(const Data& l, const Data& r) {
         len = l.len + r.len;
         sum = l.sum + r.sum;
-        lazy = 0;
     }
     void store(long long delta) { lazy += delta; }
-    void push(data &l, data &r) { l.store(lazy), r.store(lazy); }
+    void push(Data &l, Data &r) { l.store(lazy), r.store(lazy); }
     void apply() { sum += len * lazy; lazy = 0; }
 };
